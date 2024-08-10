f50004d306786aa118dca86d45f25fa686cf3726
Demi Adisa <demiadisa@gmail.com>
2024-08-10 01:21:07 -0400

    New COmponent Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { GameModesComponent } from "./game-modes/game-modes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GameModesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/game-modes/game-modes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-modes',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-modes.component.html',
+  styleUrl: './game-modes.component.scss'
+})
+export class GameModesComponent {
+
+}
